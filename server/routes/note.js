70767fde70ee64f9525e98021272931e8994a31b
JangSol1484 <dudwls1484@gmail.com>
2018-11-25 22:46:14 +0900

    기능구현, note, findUserByID
    note 테스트 해봐야함.
    user id find 검색 기능 추가

--- server/routes/note.js
+++ server/routes/note.js
@@ -2,10 +2,40 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 const db = require('../db');
+const auth = require('../auth');
+
+router.get('/video', function (req, res,next) {
+    db.getNote((err,result) => {
+        //console.log(result);
+        res.json(result);
+    })
+});
+
+router.get('/test', (req,res) => {
+    res.sendFile(path.join(__dirname, '../public', 'video.html'))
+});
 
 router.post('/video', function(req,res) {
+    let text = req.body.txt;
 
+    let substr=[];
+    let stamp=[];
+
+    text = text.replace(/\n/gi,'');
+    text = text.replace(/\r/gi,'');
+
+    console.log(text);
+    let tmp = text.split('#');
+
+    for(index = 1; index < tmp.length; index++){
+        stamp[index-1] = tmp[index].substring(0,6);
+        substr[index-1] = tmp[index].substring(6, tmp[index].length);
+        }
+        console.log(tmp.length);
+
+    for(let i=0; i< tmp.length-1; i++)
+       db.insertNote('ac3456','1',substr[i],stamp[i]); //현재 로그인한 세션의 유저 정보, 보고있는 비디오정보 받아와야.
+   
+    res.send('hello');
 })
-
-
 module.exports = router;
--- server/routes/search.js
+++ server/routes/search.js
@@ -8,13 +8,15 @@ router.get('/lecture', (req, res, next) => {
   db.searchLectureByTitle(part, (err, result) => {
     res.send(result);
   })
+
 });
 
 router.get('/user', (req, res, next) => {
   let part = req.query.query;
   db.searchUserByName(part, (err, result) => {
+   // console.log(err);
     res.send(result);
   })
-})
+});
 
 module.exports = router;
